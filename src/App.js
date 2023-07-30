4b676500e08e5b2f7242bbb1ad45017bfbad6ddd
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-07-30 22:59:42 +0300

    Add route protection

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, { action as authAction } from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout';
-import { tokenLoader } from './util/auth';
+import { tokenLoader, checkAuthLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
@@ -49,13 +49,15 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
-              }
+                loader: checkAuthLoader,
+              },
             ],
           },
           {
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader,
           },
         ],
       },
--- src/util/auth.js
+++ src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from 'react-router-dom';
+
 export const getAuthToken = () => {
   const token = localStorage.getItem('token');
 
@@ -6,4 +8,14 @@ export const getAuthToken = () => {
 
 export const tokenLoader = () => {
   return getAuthToken();
+};
+
+export const checkAuthLoader = () => {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect('/auth');
+  }
+
+  return null;
 };
