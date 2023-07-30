00fbf09dc522e4190b258e61a1aab196e3076c21
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-07-30 23:45:58 +0300

    Add automatic logout

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,10 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 // import { Outlet, useNavigation } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 const RootLayout = () => {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' })
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
 
   return (
     <>
